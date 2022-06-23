c44fefc12d695a8a194137e97fb5775f37266453
1chooo <hugo970217@gmail.com>
2022-06-23 19:32:49 +0800

    Coding2

--- Coding2/Coding2A.java
+++ Coding2/Coding2A.java
@@ -1,5 +1,12 @@
 package Coding2;
 
+import java.util.Scanner;
+
 public class Coding2A {
+    public static void main(String[] Args) {
+        Scanner myObj = new Scanner(System.in);
+
+        myObj.close();
+    }
     
 }
