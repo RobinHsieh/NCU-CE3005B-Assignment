6ee3acc31963d8ab9c03ae843631a8680d436ae8
1chooo <hugo970217@gmail.com>
2022-06-24 19:05:43 +0800

    Coding4

--- Coding4/Coding4C.java
+++ Coding4/Coding4C.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Coding4C {
     public static void main(String[] Args) {
         Scanner myObj = new Scanner(System.in);
-
+        
+        int runTimes = myObj.nextInt();
         myObj.close();
     }    
 }
