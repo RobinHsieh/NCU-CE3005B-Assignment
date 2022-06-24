4764999754feb27842e64465a3c68e2ccb2186ed
1chooo <hugo970217@gmail.com>
2022-06-24 16:29:26 +0800

    Coding3

--- Coding3/Coding3C.java
+++ Coding3/Coding3C.java
@@ -1,5 +1,16 @@
 package Coding3;
 
+import java.util.Scanner;
+
 public class Coding3C {
-    
+    public static void main(String[] Args) {
+        Scanner myObj = new Scanner(System.in);
+
+        myObj.close();
+    }    
+}
+
+
+class NumberSort {
+
 }
